b9038fb1fd47f2dc3a7b7045c7b3181775d5f38c
Tangatarov Nurzhan <tangatarovnurzan2@gmail.com>
2024-01-04 17:14:55 +0600

    Reply buttons in added

--- core/handlers/basic.py
+++ core/handlers/basic.py
@@ -2,16 +2,27 @@ from aiogram import Bot
 from aiogram.types import Message 
 import json
 
+from core.keybords.reply import reply_keyboard, loc_tel_poll_keyboard, get_reply_keyboard
+
 
 async def get_start(message: Message, bot: Bot):
-  await bot.send_message(message.from_user.id, f"<b>Привет {message.from_user.username}. Рад тебя видеть!</b>")
-  await message.answer(f"<s>Привет {message.from_user.username}. Рад тебя видеть!</s>")
-  await message.reply(f"<tg-spoiler>Привет {message.from_user.username}. Рад тебя видеть!</tg-spoiler>")
+  # await bot.send_message(message.from_user.id, f"<b>Привет {message.from_user.username}. Рад тебя видеть!</b>")
+  # await message.reply(f"<tg-spoiler>Привет {message.from_user.username}. Рад тебя видеть!</tg-spoiler>")
+  await message.answer(
+    f"<s>Привет {message.from_user.username}. Рад тебя видеть!</s>", 
+    reply_markup=get_reply_keyboard()
+  )
+
+
+async def get_location(message: Message, bot: Bot):
+  await message.answer(f'Ты оправил локацию!\r\n{message.location.latitude}\r\a{message.location.longitude}')
+
 
 async def get_photo(message: Message, bot: Bot):
   await message.answer(f"Отлично. ")
   file = await bot.get_file(message.photo[-1].file_id)
   await bot.download_file(file.file_path, 'public/img/photo.jpg')
+
 
 async def get_hello(message: Message, bot: Bot):
   await message.answer(f"И тебе привет!")
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message
 from aiogram.filters import Command
 
-from core.handlers.basic import get_start, get_photo, get_hello
+from core.handlers.basic import get_start, get_photo, get_hello, get_location
 from core.handlers.contact import get_fake_contact, get_true_contact
 from core.filters.iscontact import IsTrueContact
 from core.utils.commands import set_commands 
@@ -21,6 +21,9 @@ async def stop_bot(bot: Bot):
 def contact_type(message: Message):
   return message.contact
 
+def location_type(message: Message):
+  return message.location
+
 async def main(): 
   logging.basicConfig(
     level=logging.INFO,
@@ -32,6 +35,7 @@ async def main():
   dp.message.register(get_start, Command(commands=['start', 'run']))
   dp.message.register(get_photo, F.photo)
   dp.message.register(get_hello, F.text == 'Привет')
+  dp.message.register(get_location, location_type)
   dp.message.register(get_true_contact, contact_type, IsTrueContact())
   dp.message.register(get_fake_contact, contact_type)
   dp.startup.register(start_bot)
